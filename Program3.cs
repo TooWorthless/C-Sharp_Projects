88c4da3263b97b49755760168e91af8029d3b0ae
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2022-09-17 16:21:35 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Malikov Vadym; Group: PD-22; Option: 11
+
+namespace Lab3
+{
+    class Program 
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+            System.Console.WriteLine("Введіть число для перевірки: ");
+            string numberToCheck = Console.ReadLine();
+            bool isTrue = true;
+
+
+            for (int letterIndex = 0; letterIndex < numberToCheck.Length / 2; letterIndex++) 
+            {
+                if (numberToCheck[letterIndex] == numberToCheck[numberToCheck.Length - 1 - letterIndex])
+                {
+                    continue;
+                }
+                else {
+                    isTrue = false;
+                    break;
+                }
+            }
+
+
+            if (isTrue) {
+                System.Console.WriteLine("Введене число читається однаково зліва на право і з права на ліво");
+            }
+            else {
+                System.Console.WriteLine("Введене число НЕ читається однаково зліва на право і з права на ліво");
+            }
+        }
+    }
+}
