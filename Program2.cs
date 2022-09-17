b3d87179f7bf3db03b7441a5ad2959e9cb831d5c
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2022-09-17 16:05:19 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Malikov Vadym; Group: PD-22; Option: 11
+
+namespace Lab2 
+{
+    class Program 
+    {
+        public static void Main() 
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+            System.Console.WriteLine("Введіть максимальне число nk: ");
+            int nk = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Введіть мінімальне число nn: ");
+            int nn = int.Parse(Console.ReadLine());
+
+
+            if (nk >= nn && nn >= 0)
+            {
+                int k = nn;
+                double result = 1.0;
+
+
+                while( k <= nk )
+                {
+                    result *= (  (Math.Pow((-1), (k*k - 2*k + 3))*k + 1)  /  (k*k - 2.0)  );
+                    k++;
+                }
+
+
+                System.Console.WriteLine(result);
+            }
+        }
+    }
+}
